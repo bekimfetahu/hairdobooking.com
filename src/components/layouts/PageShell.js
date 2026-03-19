74726bddf98749a3940c6ec1c3222ff4662c1dbb
bekim <bekim.fetahu@gmail.com>
2026-03-19 20:26:28 +0000

    Update PageShell container padding; increase bottom padding for improved layout consistency

--- src/components/layouts/PageShell.js
+++ src/components/layouts/PageShell.js
@@ -15,7 +15,7 @@ export default function PageShell({
     actions,
     variant = "default",
     className = "",
-    containerClassName = "mx-auto max-w-6xl px-6 py-5 sm:py-3",
+    containerClassName = "mx-auto max-w-6xl px-6 py-5 sm:py-10",
     contentClassName = "",
     headerClassName = "",
     titleClassName = "",
