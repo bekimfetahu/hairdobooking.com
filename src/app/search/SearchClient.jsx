2d402b8a28b8cead1ffb6fa521c38a3094d4d156
bekim <bekim.fetahu@gmail.com>
2026-03-28 21:23:39 +0000

    feat: create SearchClient component for handling salon search parameters

---
+++ src/app/search/SearchClient.jsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+import PreferredSalonSearch from "@/components/PreferredSalonSearch";
+
+export default function SearchClient() {
+    const searchParams = useSearchParams();
+    const initialService = searchParams.get("service") || "";
+    const initialLocation = searchParams.get("location") || "";
+
+    const initialSearch = initialLocation || initialService || "";
+
+    return <PreferredSalonSearch initialSearch={initialSearch} />;
+}
